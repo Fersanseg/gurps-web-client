99ca6bbc0703eb52290da20feacd5f23b9e18c32
Fersanseg <fer_ssa94@hotmail.com>
2024-03-03 16:47:06 +0100

    Pass Authorization header on secure request

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,18 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { map } from 'rxjs';
+import { LocalService } from './local.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private localService: LocalService
+  ) { }
 
   createTrait() {
     const data = {};
-    return this.http.post(`${environment.serverUrl}/app/insert`, data)
+    const headers = new HttpHeaders({
+      "Authorization": `Bearer ${this.localService.get("token") || ""}`
+    });
+    return this.http.post(`${environment.serverUrl}/app/db/insert`, data, {headers, responseType: 'text'})
       .pipe(
         map(res => this._handleInsert(res))
       );
