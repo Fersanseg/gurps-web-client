27acd53e890e747f1e8ee3f5ec0101a3345da4d8
Fersanseg <fer_ssa94@hotmail.com>
2024-03-05 20:32:44 +0100

    Title animation

---
+++ src/app/animations/fade.ts
@@ -0,0 +1,11 @@
+import { animate, style, transition, trigger } from "@angular/animations";
+
+export const fadeInAnimation = trigger('fadeIn', [
+    transition(':enter', [
+        style({
+            opacity: 0,
+            transform: 'translateY(100px)'
+        }),
+        animate('700ms ease-out', style({opacity: 1, transform: 'none'}))
+    ])
+])
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { InsertComponent } from "../insert/insert.component";
+import { fadeInAnimation } from '../../animations/fade';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [RouterLink, RouterLinkActive, CommonModule, InsertComponent]
+    imports: [RouterLink, RouterLinkActive, CommonModule, InsertComponent],
+    animations: [fadeInAnimation]
 })
 export class HomeComponent {
+    public onAfterTitleEnter: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+    onTitleFadeIn() {
+        this.onAfterTitleEnter.next(true);
+    }
 
 }
