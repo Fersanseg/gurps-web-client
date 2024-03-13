ebec9da1474e62a74192be427d0a835e46a26737
Fersanseg <fer_ssa94@hotmail.com>
2024-03-13 21:27:12 +0100

    splitter 1

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { SplitterComponent } from "../../components/splitter/splitter.component";
 
 @Component({
     selector: 'app-info',
     standalone: true,
     templateUrl: './info.component.html',
     styleUrl: './info.component.css',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, SplitterComponent]
 })
 export class InfoComponent {
 
