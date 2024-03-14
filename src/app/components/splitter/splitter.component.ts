f374a8f2385c746d0965c7a8bd3cc122b25f7b48
Fersanseg <fer_ssa94@hotmail.com>
2024-03-14 21:14:22 +0100

    flipped splitter

--- src/app/components/splitter/splitter.component.ts
+++ src/app/components/splitter/splitter.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-splitter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './splitter.component.html',
   styleUrl: './splitter.component.css'
 })
 export class SplitterComponent {
-
+  @Input() public flipped = false;
 }
