c45005f8b9e8e98fc912d1a85043f78b0e5947e5
Fersanseg <fer_ssa94@hotmail.com>
2024-03-17 21:29:39 +0100

    Game info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { InfoComponent } from './pages/info/info.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent, data: {animation: 'homeComponent'}},
-    {path: 'login', component: LoginComponent, data: {animation: 'loginComponent'}},
-    {path: 'info', component: InfoComponent, data: {animation: 'infoComponent'}}
+  {path: '', component: HomeComponent, data: {animation: 'homeComponent'}},
+  {path: 'login', component: LoginComponent, data: {animation: 'loginComponent'}},
+  {path: 'info', component: InfoComponent, data: {animation: 'infoComponent'}},
+  {path: '**', component: HomeComponent, data: {animation: 'homeComponent'}}
 ];
