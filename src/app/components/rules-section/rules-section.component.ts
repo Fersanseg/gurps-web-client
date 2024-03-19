e5f8ad4cb538737e764566185e0d8cfa999c4509
Fersanseg <fer_ssa94@hotmail.com>
2024-03-19 21:02:06 +0100

    dummy content 'Rules section' component

--- src/app/components/rules-section/rules-section.component.ts
+++ src/app/components/rules-section/rules-section.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-rules-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './rules-section.component.html',
   styleUrl: './rules-section.component.css'
 })
 export class RulesSectionComponent {
-
+  @Input() public sectionTitle!: string;
+  @Input() public pageRef?: string;
 }
--- src/app/pages/rules/rules.component.ts
+++ src/app/pages/rules/rules.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { SplitterComponent } from "../../components/splitter/splitter.component";
+import { RulesSectionComponent } from "../../components/rules-section/rules-section.component";
 
 @Component({
     selector: 'app-rules',
     standalone: true,
     templateUrl: './rules.component.html',
     styleUrl: './rules.component.css',
-    imports: [HeaderComponent, SplitterComponent]
+    imports: [HeaderComponent, SplitterComponent, RulesSectionComponent]
 })
 export class RulesComponent {
 
