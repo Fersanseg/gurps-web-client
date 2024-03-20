30799af33105ffdb87195d6efd70996f468d6c50
Fersanseg <fer_ssa94@hotmail.com>
2024-03-20 19:44:26 +0100

    Added expand animation to rules section

--- src/app/components/rules-section/rules-section.component.ts
+++ src/app/components/rules-section/rules-section.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { expandAnimation } from '../../animations/expand-height';
 
 @Component({
   selector: 'app-rules-section',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './rules-section.component.html',
-  styleUrl: './rules-section.component.css'
+  styleUrl: './rules-section.component.css',
+  animations: [expandAnimation]
 })
 export class RulesSectionComponent {
   @Input() public sectionTitle!: string;
   @Input() public pageRef?: string;
+
+  public isShown: boolean = false;
+
+  toggleSection() {
+    this.isShown = !this.isShown;
+  }
 }
