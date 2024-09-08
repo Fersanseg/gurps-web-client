4d5debf853b9c2b38792b453d660473238387d52
Fersanseg <fer_ssa94@hotmail.com>
2024-09-08 21:34:38 +0200

    Always show header except in home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
-import { ChildrenOutletContexts, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ChildrenOutletContexts, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { routeFadeInOutAnimation } from './animations/route-transition';
+import { HeaderComponent } from "./components/header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   animations: [routeFadeInOutAnimation]
 })
 export class AppComponent {
-  title = 'front';
-  constructor(private contexts: ChildrenOutletContexts) {}
+  get showHeader(): boolean { 
+    console.log(this._router.url)
+    return this._router.url != "/";
+  }
+
+  constructor(
+    private contexts: ChildrenOutletContexts,
+    private _router: Router
+  ) {}
 
   getRouteAnimationData() {
     return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
